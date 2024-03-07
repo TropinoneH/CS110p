c9a3232c869be2102b203fbbf2cebcf24452ad42
Tropinone <mspt5871@protonmail.com>
2024-03-07 19:48:39 +0800

    task 2, sub task 1: malloc and realloc

--- vector.c
+++ vector.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 
 Vector *vector_create(void) {
-  Vector *vector = malloc(sizeof(Vector));
-  vector->size = 0;
-  vector->capacity = VECTOR_INITIAL_CAPACITY;
-  vector->data = malloc(sizeof(double) * vector->capacity);
-  return vector;
+    Vector *vector = malloc(sizeof(Vector));
+    if (vector == NULL) {
+        return NULL;
+    }
+    vector->size = 0;
+    vector->capacity = VECTOR_INITIAL_CAPACITY;
+    vector->data = malloc(sizeof(double) * vector->capacity);
+    if (vector->data == NULL) {
+        free(vector);
+        return NULL;
+    }
+    return vector;
 }
 
 void vector_push(Vector *vector, double element) {
-  if (vector->size == vector->capacity) {
-    vector->capacity *= 2;
-    vector->data = realloc(vector->data, sizeof(double) * vector->capacity);
-  }
-  vector->data[vector->size++] = element;
+    if (vector->size == vector->capacity) {
+        vector->capacity *= 2;
+        if (vector->data == NULL) {
+            vector->data = malloc(sizeof(double) * vector->capacity);
+        } else {
+            vector->data =
+                realloc(vector->data, sizeof(double) * vector->capacity);
+        }
+    }
+    vector->data[vector->size++] = element;
 }
 
 double vector_get(const Vector *vector, int index) {
-  return vector->data[index];
+    return vector->data[index];
 }
 
 void vector_free(Vector *vector) {
-  free(vector->data);
-  free(vector);
+    free(vector->data);
+    free(vector);
 }

670011ab25c559e3ba45323f7162740b326a1c25
Tropinone <mspt5871@protonmail.com>
2024-03-10 19:19:51 +0800

    add function entry in vector.h

--- vector.h
+++ vector.h
@@ -4,9 +4,9 @@
 #define VECTOR_INITIAL_CAPACITY 10
 
 typedef struct {
-  double *data;
-  int size;
-  int capacity;
+    double *data;
+    int size;
+    int capacity;
 } Vector;
 
 Vector *vector_create(void);
@@ -16,5 +16,7 @@ void vector_push(Vector *vector, double element);
 double vector_get(const Vector *vector, int index);
 
 void vector_free(Vector *vector);
+
+void another_vector_free(Vector **vector);
 
 #endif // LAB_2_VECTOR_H

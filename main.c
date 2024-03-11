a6b6fc659910f5b36fd5e6a2bd11d70b6722293c
Tropinone <mspt5871@protonmail.com>
2024-03-11 10:26:20 +0800

    formatting

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 int main(void) {
-  Vector *vector = vector_create();
-  for (int i = 0; i < 100; ++i) {
-    vector_push(vector, i * 10 / 7.0);
-  }
-  for (int i = 0; i < 100; ++i) {
-    printf("%f\n", vector_get(vector, i));
-  }
-  vector_free(vector);
-  return 0;
+    Vector *vector = vector_create();
+    for (int i = 0; i < 100; ++i) {
+        vector_push(vector, i * 10 / 7.0);
+    }
+    for (int i = 0; i < 100; ++i) {
+        printf("%f\n", vector_get(vector, i));
+    }
+    vector_free(vector);
+    return 0;
 }

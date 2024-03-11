13758ec128de15496a46e16b903ea735a1364ca0
Tropinone <mspt5871@protonmail.com>
2024-03-11 10:55:04 +0800

    task 2, subtask 1 and 2: fixed bugs

--- vector.c
+++ vector.c
@@ -1,15 +1,18 @@
 #include "vector.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 Vector *vector_create(void) {
     Vector *vector = malloc(sizeof(Vector));
     if (vector == NULL) {
-        return NULL;
+        printf("Invalid vector\n");
+        exit(1);
     }
     vector->size = 0;
     vector->capacity = VECTOR_INITIAL_CAPACITY;
     vector->data = malloc(sizeof(double) * vector->capacity);
     if (vector->data == NULL) {
+        printf("Memory allocation failed\n");
         free(vector);
         return NULL;
     }
@@ -18,38 +21,44 @@ Vector *vector_create(void) {
 
 void vector_push(Vector *vector, double element) {
     if (vector == NULL) {
-        return;
+        printf("Invalid vector\n");
+        exit(1);
     }
     if (vector->size == vector->capacity) {
         vector->capacity *= 2;
-        if (vector->data == NULL) {
-            vector->data = malloc(sizeof(double) * vector->capacity);
-        } else {
-            vector->data =
-                realloc(vector->data, sizeof(double) * vector->capacity);
+        double *tmp = realloc(vector->data, sizeof(double) * vector->capacity);
+        if (tmp == NULL) {
+            printf("Memory allocation failed\n");
+            free(vector->data);
+            free(vector);
+            exit(1);
         }
+        vector->data = tmp;
     }
     vector->data[vector->size++] = element;
 }
 
 double vector_get(const Vector *vector, int index) {
     if (vector == NULL || index < 0 || index >= vector->size) {
-        return 0;
+        printf("Invalid index\n");
+        exit(1);
     }
     return vector->data[index];
 }
 
 void vector_free(Vector *vector) {
     if (vector == NULL) {
-        return;
+        printf("Invalid vector\n");
+        exit(1);
     }
     free(vector->data);
     free(vector);
 }
 
 void another_vector_free(Vector **vector) {
-    if (*vector == NULL || vector == NULL) {
-        return;
+    if (vector == NULL || *vector == NULL) {
+        printf("Invalid vector\n");
+        exit(1);
     }
     free((*vector)->data);
     free(*vector);

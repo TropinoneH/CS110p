ad0b0ba6624e6298bc226211406103cf74903b84
malj <malj@shanghaitech.edu.cn>
2024-03-19 10:34:13 +0800

    p1.1 framework

---
+++ inc/assembler.h
@@ -0,0 +1,12 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* DO NOT MODIFY GIVEN API*/
+int assembler(FILE *input_file, FILE *output_file);
+
+#endif
---
+++ src/assembler.c
@@ -0,0 +1,9 @@
+#include "../inc/assembler.h"
+#include "../inc/util.h"
+
+/* DO NOT MODIFY THE GIVEN API*/
+int assembler(FILE *input_file,
+              FILE *output_file) {
+  /*YOUR CODE HERE*/
+  return 0;
+}

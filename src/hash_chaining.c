d370564a09565ff4c65fc152750ae19700f7382a
Letong Han <hanlt@shanghaitech.edu.cn>
2024-03-28 10:36:10 +0800

    initial commit

---
+++ src/hash_chaining.c
@@ -0,0 +1,18 @@
+#include "../inc/hash_chaining.h"
+#include "../inc/hash_func.h"
+
+hash_chaining *hash_chaining_init(uint32_t size) {
+  // TODO
+}
+
+void hash_chaining_insert(hash_chaining *table, uint32_t key) {
+  // TODO
+}
+
+bool hash_chaining_search(hash_chaining *table, uint32_t key) {
+  // TODO
+}
+
+void hash_chaining_destroy(hash_chaining *table) {
+  // TODO
+}

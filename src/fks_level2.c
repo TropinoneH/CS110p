efd925bcad2ea84e9b39bed23bb01a72c1639748
TropinoneH <mspt5871@protonmail.com>
2024-04-02 14:52:49 +0800

    finish error, didn't pass the test_fks.c

--- src/fks_level2.c
+++ src/fks_level2.c
@@ -3,22 +3,42 @@
 #include <string.h>
 
 fks_level2 *fks_level2_init(uint32_t size, hash_parameters parameters) {
-    // TODO
+    fks_level2 *table = (fks_level2 *)malloc(sizeof(fks_level2));
+    table->size = size;
+    table->parameters = parameters;
+    table->slots = malloc(sizeof(uint32_t) * size);
+    memset(table->slots, FKS_LEVEL2_EMPTY, sizeof(uint32_t) * size);
+    return table;
 }
 
 fks_level2 *fks_level2_build(list_node *head, uint32_t size,
                              hash_parameters parameters) {
-    // TODO
+    if (size == 0) {
+        return NULL;
+    }
+    fks_level2 *table = fks_level2_init(size, parameters);
+    while (head) {
+        fks_level2_insert(table, head->key);
+        head = head->next;
+    }
+    return table;
 }
 
 bool fks_level2_insert(fks_level2 *table, uint32_t key) {
-    // TODO
+    uint32_t index = hash_func(key, table->parameters, table->size);
+    if (table->slots[index] != FKS_LEVEL2_EMPTY) {
+        return false;
+    }
+    table->slots[index] = key;
+    return true;
 }
 
 bool fks_level2_search(fks_level2 *table, uint32_t key) {
-    // TODO
+    uint32_t index = hash_func(key, table->parameters, table->size);
+    return table->slots[index] == key;
 }
 
 void fks_level2_destroy(fks_level2 *table) {
-    // TODO
+    free(table->slots);
+    free(table);
 }
--- src/hash_chaining.c
+++ src/hash_chaining.c
@@ -2,17 +2,43 @@
 #include "../inc/hash_func.h"
 
 hash_chaining *hash_chaining_init(uint32_t size) {
-    // TODO
+    hash_chaining *table = (hash_chaining *)malloc(sizeof(hash_chaining));
+    table->slots = calloc(size, sizeof(list_node *));
+    table->size = size;
+    table->parameters = generate_hash_parameters();
+    return table;
 }
 
 void hash_chaining_insert(hash_chaining *table, uint32_t key) {
-    // TODO
+    uint32_t index = hash_func(key, table->parameters, table->size);
+    list_node *node = (list_node *)malloc(sizeof(list_node));
+    node->key = key;
+    node->next = table->slots[index];
+    table->slots[index] = node;
 }
 
 bool hash_chaining_search(hash_chaining *table, uint32_t key) {
-    // TODO
+    uint32_t index = hash_func(key, table->parameters, table->size);
+    list_node *node = table->slots[index];
+    while (node) {
+        if (node->key == key) {
+            return true;
+        }
+        node = node->next;
+    }
+    return false;
 }
 
 void hash_chaining_destroy(hash_chaining *table) {
-    // TODO
+    for (uint32_t i = 0; i < table->size; ++i) {
+        list_node *node = table->slots[i];
+        while (node) {
+            list_node *temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+
+    free(table->slots);
+    free(table);
 }

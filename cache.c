66da4d46478b08c2acb1ec87a42b95b8225dabe3
TropinoneH <mspt5871@protonmail.com>
2024-05-26 19:31:50 +0800

    finish cache_create

--- cache.c
+++ cache.c
@@ -2,34 +2,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <math.h>
+
+/* utils */
+uint32_t mlog2(double x) {
+    uint32_t res = 0;
+    while (x > .001) {
+        x /= 2;
+        res++;
+    }
+    return res;
+}
+
 
 /* Create a cache simulator according to the config */
 struct cache *cache_create(struct cache_config config, struct cache *lower_level) {
     /*YOUR CODE HERE*/
-    struct cache *c = (struct cache *) malloc(sizeof(struct cache));
-    c->config = config;
-    c->lower_cache = lower_level;
+    struct cache *cache = (struct cache *) malloc(sizeof(struct cache));
+    cache->config = config;
+    cache->lower_cache = lower_level;
 
-    c->offset_bits = (uint32_t) round(log2(c->config.line_size));
-    c->index_bits = (uint32_t) round(log2((double) c->config.lines / c->config.ways));
-    c->tag_bits = config.address_bits - c->offset_bits - c->index_bits;
+    cache->offset_bits = mlog2(cache->config.line_size);
+    cache->index_bits = mlog2((double) cache->config.lines / cache->config.ways);
+    cache->tag_bits = config.address_bits - cache->offset_bits - cache->index_bits;
 
-    c->offset_mask = (1 << c->offset_bits) - 1;
-    c->index_mask = ((1 << c->index_bits) - 1) << c->offset_bits;
-    c->tag_mask = ((1 << c->tag_bits) - 1) << (c->offset_bits + c->index_bits);
+    cache->offset_mask = (1 << cache->offset_bits) - 1;
+    cache->index_mask = ((1 << cache->index_bits) - 1) << cache->offset_bits;
+    cache->tag_mask = ((1 << cache->tag_bits) - 1) << (cache->offset_bits + cache->index_bits);
 
-    c->lines = (struct cache_line *) malloc(sizeof(struct cache_line) * c->config.lines);
-    for (uint32_t i = 0; i < c->config.lines; ++i) {
-        c->lines[i].valid = false;
-        c->lines[i].dirty = false;
-        c->lines[i].tag = 0;
-        c->lines[i].last_access = 0;
-        c->lines[i].data = (uint8_t *) malloc(sizeof(uint8_t) * c->config.line_size);
-        memset(c->lines[i].data, 0, sizeof(uint8_t) * c->config.line_size);
+    cache->lines = (struct cache_line *) malloc(sizeof(struct cache_line) * cache->config.lines);
+    if (!cache->lines)
+    {
+        free(cache);
+        return NULL;
     }
 
-    return c;
+    for (uint32_t i = 0; i < cache->config.lines; ++i) {
+        cache->lines[i].valid = false;
+        cache->lines[i].dirty = false;
+        cache->lines[i].tag = 0;
+        cache->lines[i].last_access = 0;
+        cache->lines[i].data = (uint8_t *) calloc(cache->config.line_size, sizeof(uint8_t));
+
+        if (!cache->lines[i].data)
+        {
+            for (size_t j = 0; j != i; ++j)
+                free(cache->lines[j].data);
+            free(cache->lines);
+            free(cache);
+            return NULL;
+        }
+    }
+
+    return cache;
 }
 
 /* 
@@ -44,37 +68,15 @@ and so on.
 */
 void cache_destroy(struct cache *cache) {
     /*YOUR CODE HERE*/
-    for (uint32_t i = 0; i < cache->config.lines; ++i) {
-        if (cache->lines[i].dirty) {
-            if (cache->lower_cache != NULL) {
-                uint32_t addr = (cache->lines[i].tag << (cache->config.address_bits - cache->tag_bits)) |
-                                (i << cache->offset_bits);
-                for (uint32_t j = 0; j < cache->config.line_size; ++j)
-                    cache_write_byte(cache->lower_cache, addr + j, cache->lines[i].data[j]);
-            } else {
-                mem_store(cache->lines[i].data, cache->config.line_size, cache->lines[i].tag << cache->offset_bits);
-            }
-        }
-        free(cache->lines[i].data);
-    }
-
-    free(cache->lines);
-    free(cache);
 }
 
 /* Read one byte at a specific address. return hit=true/miss=false */
 bool cache_read_byte(struct cache *cache, uint32_t addr, uint8_t *byte) {
     /*YOUR CODE HERE*/
-    cache->lines[0].last_access = get_timestamp();
-    addr += 1;
-    byte += 1;
     return true;
 }
 /* Write one byte into a specific address. return hit=true/miss=false*/
 bool cache_write_byte(struct cache *cache, uint32_t addr, uint8_t byte) {
     /*YOUR CODE HERE*/
-    cache->lines[0].last_access = get_timestamp();
-    addr += 1;
-    byte += 1;
     return true;
 }

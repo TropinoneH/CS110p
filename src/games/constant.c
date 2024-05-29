b35932c18294a8e5f30883b439cf4ccd7b7df622
TropinoneH <mspt5871@protonmail.com>
2024-05-29 11:36:50 +0800

    add global constant

---
+++ src/games/constant.c
@@ -0,0 +1,13 @@
+//
+// Created by mspt5 on 24-5-29.
+//
+
+#include "games/constant.h"
+#include "utils.h"
+
+int cur_level = 0;
+
+int V_JOY_UP = JOY_LEFT;
+int V_JOY_DOWN = JOY_RIGHT;
+int V_JOY_LEFT = JOY_DOWN;
+int V_JOY_RIGHT = JOY_UP;

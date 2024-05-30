88b4ec5bc873105430b92f13bf9a3c87eccc6af4
TropinoneH <mspt5871@protonmail.com>
2024-05-30 08:23:50 +0800

    start play scene

---
+++ src/games/play.c
@@ -0,0 +1,16 @@
+//
+// Created by mspt5 on 24-5-29.
+//
+#include "games/play.h"
+//#include "lcd/lcd.h"
+
+void play_loop() {
+    // TODO: draw player info: health, score
+
+    // TODO: draw bird with speed down
+    // TODO: draw line behind bird
+    // TODO: draw wall
+
+    // TODO: judge too low or too high
+    // TODO: judge hit wall
+}

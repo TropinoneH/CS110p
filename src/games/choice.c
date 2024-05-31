f05607da5b558495389678c7563d146f7937f4e2
TropinoneH <mspt5871@protonmail.com>
2024-05-31 22:32:46 +0800

    change choice menu for new standard

--- src/games/choice.c
+++ src/games/choice.c
@@ -18,24 +18,25 @@ void draw_menu(int level) {
 }
 
 void choose_level() {
-    bool hold = false;
+    uint64_t up = 0;
+    uint64_t down = 0;
     draw_menu(cur_level);
     while (!Get_Button(JOY_CTR)) {
-        if (Get_Button(V_JOY_UP) && cur_level && !hold) {
-            hold = true;
-            cur_level -= 1;
+        if (Get_Button(V_JOY_UP)) {
+            if (get_timer_value() - up < SystemCoreClock / 4000 * 300) continue;
+            up = get_timer_value();
+            cur_level = cur_level == 0 ? 3 : cur_level - 1;
             draw_menu(cur_level);
-        } else if (Get_Button(V_JOY_DOWN) && cur_level != 3 && !hold) {
-            hold = true;
-            cur_level += 1;
+        } else if (Get_Button(V_JOY_DOWN)) {
+            if (get_timer_value() - down < SystemCoreClock / 4000 * 300) continue;
+            down = get_timer_value();
+            cur_level = cur_level == 3 ? 0 : cur_level + 1;
             draw_menu(cur_level);
         }
-
-        if (!Get_Button(V_JOY_UP) && !Get_Button(V_JOY_DOWN)) hold = false;
     }
 
     if (cur_level == 3) {
-        startup_scene();
+        if (startup_scene(0)) exit(1);
         choose_level();
     }
 }

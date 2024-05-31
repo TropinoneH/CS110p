8ca2c035cf99cfb1966aaf25fbcfb3328f1af17a
TropinoneH <mspt5871@protonmail.com>
2024-06-01 03:21:01 +0800

    add flash for choice menu

--- src/games/choice.c
+++ src/games/choice.c
@@ -21,7 +21,7 @@ void choose_level() {
     uint64_t up = 0;
     uint64_t down = 0;
     draw_menu(cur_level);
-    while (!Get_Button(JOY_CTR)) {
+    while (!Get_Button(V_JOY_CTR)) {
         if (Get_Button(V_JOY_UP)) {
             if (get_timer_value() - up < SystemCoreClock / 4000 * 300) continue;
             up = get_timer_value();
@@ -34,6 +34,48 @@ void choose_level() {
             draw_menu(cur_level);
         }
     }
+
+    switch (cur_level) {
+        case 0:
+            LCD_ShowString(22, 0, (u8 *) "easy", BLACK);
+            break;
+        case 1:
+            LCD_ShowString(16, 20, (u8 *) "normal", BLACK);
+            break;
+        case 2:
+            LCD_ShowString(22, 40, (u8 *) "hard", BLACK);
+            break;
+        case 3:
+            LCD_ShowString(18, 80, (u8 *) "debug", BLACK);
+            break;
+    }
+    for (int i = 0; i < 10; i++) {
+        switch (cur_level) {
+            case 0:
+                LCD_ShowString(22, 0, (u8 *) "easy", GRAY);
+                delay_1ms(50);
+                LCD_ShowString(22, 0, (u8 *) "easy", WHITE);
+                break;
+            case 1:
+                LCD_ShowString(16, 20, (u8 *) "normal", GRAY);
+                delay_1ms(50);
+                LCD_ShowString(16, 20, (u8 *) "normal", WHITE);
+                break;
+            case 2:
+                LCD_ShowString(22, 40, (u8 *) "hard", GRAY);
+                delay_1ms(50);
+                LCD_ShowString(22, 40, (u8 *) "hard", WHITE);
+                break;
+            case 3:
+                LCD_ShowString(18, 80, (u8 *) "debug", GRAY);
+                delay_1ms(50);
+                LCD_ShowString(18, 80, (u8 *) "debug", BROWN);
+                break;
+        }
+        delay_1ms(50);
+    }
+
+    delay_1ms(1000);
 
     if (cur_level == 3) {
         if (startup_scene(0)) exit(1);

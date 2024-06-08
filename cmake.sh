2a1cbbbd50f1cdc0deead778ecefecf0fd3a269d
Tropinone <mspt5871@protonmail.com>
2024-06-09 02:23:46 +0800

    remove absolution path

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,5 @@
-/usr/local/applications/jetbrains/clion/bin/cmake/linux/x64/bin/cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/usr/local/applications/jetbrains/clion/bin/ninja/linux/x64/ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S ./ -B ./cmake-build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -DCMAKE_C_COMPILER=gcc -G Ninja -S ./ -B ./cmake-build-debug
 
-/usr/local/applications/jetbrains/clion/bin/cmake/linux/x64/bin/cmake --build ./cmake-build-debug --target test -j 18
+cmake --build ./cmake-build-debug --target test -j 18
 
 ./cmake-build-debug/test

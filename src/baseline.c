da128510b904a827f0da2a95faa285db8e2bceaf
TropinoneH <mspt5871@protonmail.com>
2024-06-10 03:10:40 +0800

    baseline -> openmp optimized

--- src/baseline.c
+++ src/baseline.c
@@ -1,5 +1,8 @@
+#include <omp.h>
+
 void baseline(int N, int step, double *p, double *p_next) {
     for (int k = 0; k < step; k++) {
+#pragma omp parallel for
         for (int i = 1; i < N - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
                 p_next[i * N + j] = (p[(i - 1) * N + j] + p[(i + 1) * N + j] + p[i * N + j + 1] + p[i * N + j - 1]) / 4.0f;

754c7f7a33f9451fbe6a19c01a83b9f7f56eb038
TropinoneH <mspt5871@protonmail.com>
2024-06-13 17:25:01 +0800

    4-step expand

--- src/baseline.c
+++ src/baseline.c
@@ -3,8 +3,8 @@
 void baseline(int N, int step, double *p, double *p_next) {
     for (int k = 0; k < step; k++) {
 #pragma omp parallel for
-        for (int i = 1; i < N - 1; i++) {
-            for (int j = 1; j < N - 1; j++) {
+        for (int i = 4; i < N - 4; i++) {
+            for (int j = 4; j < N - 4; j++) {
                 p_next[i * N + j] = (p[(i - 1) * N + j] + p[(i + 1) * N + j] + p[i * N + j + 1] + p[i * N + j - 1]) / 4.0f;
             }
         }
--- src/impl.c
+++ src/impl.c
@@ -1,24 +1,36 @@
-#include <memory.h>
-#include <omp.h>
 #include <emmintrin.h>
 #include <immintrin.h>
+#include <memory.h>
+#include <omp.h>
+#include <string.h>
 
 void impl(int N, int step, double *p) {
     /* Your code here */
-    double *p_next = calloc(N * N, sizeof(double));
-    for (int k = 0; k < step / 2; k++) {
-        for (int i = 1; i < N - 1; i++) {
-            for (int j = 1; j < N - 1; j++) {
-                p_next[i * N + j] = (p[(i - 1) * N + j] + p[(i + 1) * N + j] + p[i * N + j + 1] + p[i * N + j - 1]) / 4.0f;
-            }
-        }
+    double *temp;
+    double *p_next = malloc(N * N * sizeof(double));
+    memcpy(p_next, p, N * N * sizeof(double));
 
-        for (int i = 1; i < N - 1; i++) {
-            for (int j = 1; j < N - 1; j++) {
-                p[i * N + j] = (p_next[(i - 1) * N + j] + p_next[(i + 1) * N + j] + p_next[i * N + j + 1] + p_next[i * N + j - 1]) / 4.0f;
+    int i, j, k;
+    for (k = 0; k < step / 4; k++) {
+        for (i = 4; i < N - 4; i++) {
+            for (j = 4; j < N - 4; j++) {
+                p_next[i * N + j] = (1.0 / 256) * (p[(i - 4) * N + (j)] + p[(i) *N + (j - 4)] + p[(i) *N + (j + 4)] + p[(i + 4) * N + (j)]) +
+                                    (1.0 / 64) * (p[(i - 3) * N + (j - 1)] + p[(i - 3) * N + (j + 1)] + p[(i - 1) * N + (j - 3)] + p[(i - 1) * N + (j + 3)] +
+                                                  p[(i + 1) * N + (j - 3)] + p[(i + 1) * N + (j + 3)] + p[(i + 3) * N + (j - 1)] + p[(i + 3) * N + (j + 1)]) +
+                                    (3.0 / 128) * (p[(i - 2) * N + (j - 2)] + p[(i - 2) * N + (j + 2)] + p[(i + 2) * N + (j - 2)] + p[(i + 2) * N + (j + 2)]) +
+                                    (1.0 / 16) * (p[(i - 2) * N + (j)] + p[(i) *N + (j - 2)] + p[(i) *N + (j + 2)] + p[(i + 2) * N + (j)]) +
+                                    (3.0 / 32) * (p[(i - 1) * N + (j - 1)] + p[(i - 1) * N + (j + 1)] + p[(i + 1) * N + (j - 1)] + p[(i + 1) * N + (j + 1)]) +
+                                    (9.0 / 64) * (p[(i) *N + (j)]);
             }
         }
+        // corner 3
+        // p_next[3 * N + 3] = (1.0/256)*(p[7*N+])
+
+        temp = p;
+        p = p_next;
+        p_next = temp;
     }
+
 
     free(p_next);
 }
